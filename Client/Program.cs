0c61d24af88cacf9baec7675e8d8d29df455382b
tide Blue <835059949@qq.com>
2021-07-07 13:40:12 +0800

    2021-7-7

---
+++ Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using BitTorrent;
+using System;
+using System.IO;
+
+namespace Program
+{
+    public class Program
+    {
+        public static Client Client;
+
+        public static void Main(string[] args)
+        {
+            int port = -1;
+
+            if(args.Length!=3||!Int32.TryParse(args[0],out port) || !File.Exists(args[1]))
+            {
+                Console.WriteLine("Error: requires port, torrent file and download directory as first, second and third arguments");
+                return;
+            }
+
+            Client = new Client(port, args[1], args[2]);
+            Client.Start();
+
+            
+            Client.Stop();
+        }
+    }
+}
